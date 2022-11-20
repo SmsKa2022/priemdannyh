f2e89e564ea6b9fee93fac768a4c1fd1f4c50eb2
SmsKa2022 <2009sms@rambler.ru>
2022-11-20 18:59:58 +0300

    Qveryparametri

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-const express = require('express')
+const  response = require('express');
+const express = require('express');
+const { request } = require('http');
 const server = express()
 const path = require('path');
 const { debugPort } = require('process');
@@ -10,19 +12,27 @@ server.use(express.urlencoded({extended:true}));
 server.use(express.static(__dirname + "/static"));
 
 
-server.get('/', function (req, res) {
-    res.render('index', {listOfPeople:db.people})
-    })
+server.get ('/', (req, resp) => {
+    const usersQuery=req.query
+    let listpeopleForRender = db.people
+    if (usersQuery.limit!=undefined && Number.isNaN(+usersQuery.limit)===false) {
+       listpeopleForRender = db.people.slice(0,usersQuery.limit)
+    }
+    resp.render('index',{listOfPeople:listpeopleForRender})
+})
+
+//server.get('/', function (req, res) {
+ //   res.render('index', {listOfPeople:db.people})
+   // })
 
 server.get ('/abc', function (req, res)
     { res.sendFile(path.join(process.cwd(), 'abc.html'))
 })
-server.get ('*', (req, res)=> {
-    res.send(`<div>
-    <h1>404</h1>
-    <a href='/'> Ступай на главную страницу и не пиши чушь</a>
-    </div>`)
+
+server.get ('/index.html', function (req, res)
+    { res.sendFile(path.join(process.cwd(), 'index.html'))
 })
+
 server.post('/adressbook', function (req,res) {
     const dataFromFrom = req.body;
     // db.people.push(dataFromFrom);
@@ -34,6 +44,9 @@ server.post('/adressbook', function (req,res) {
     });
     res.redirect('/');
 })
+server.get ('*', (req, res)=> {
+       res.render('404')
+    })
 
     // this.app.use(express.static(path.join(__dirname, "index.html")));
 server.listen(PORT)
@@ -41,3 +54,4 @@ console.log('http://127.0.0.1:3000')
 console.log('http://localhost:3000')
 console.log('http://localhost:3000/abc')
 console.log('http://localhost:3000/abcd прокатились на еже')
+console.log('http://localhost:3000/index.html')
