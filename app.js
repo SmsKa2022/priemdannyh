bff53c05f84d8460f297116ecf5b2b928b6bb96f
SmsKa2022 <2009sms@rambler.ru>
2022-11-21 21:12:18 +0300

    file

--- app.js
+++ app.js
@@ -1,39 +1,52 @@
-const express = require('express')
-const server = express()
+const express = require('express');
+const server = express();
+const fs = require('fs');
 const path = require('path');
-const { debugPort } = require('process');
-const PORT = 3000
-const {db} = require('./src/db')
+const PORT = 3000;
 server.set('view engine', 'hbs');
-server.set('views', path.join(__dirname, 'src', 'views'))
+server.set('views', path.join(__dirname, 'src', 'views'));
 server.use(express.urlencoded({extended:true}));
 server.use(express.static(__dirname + "/static"));
 
 
-server.get('/', function (req, res) {
-    res.render('index', {listOfPeople:db.people})
-    })
+server.get ('/', (req, resp) => {
+    const usersQuery = req.query;
+    let listpeopleForRender = JSON.parse(fs.readFileSync('./src/data.json'));
+    if (usersQuery.limit != undefined && Number.isNaN(+usersQuery.limit) === false) {
+       listpeopleForRender = listpeopleForRender.slice(0, usersQuery.limit);
+    }
+    resp.render('index',{listOfPeople:listpeopleForRender})
+})
+
+//server.get('/', function (req, res) {
+ //   res.render('index', {listOfPeople:db.people})
+   // })
 
 server.get ('/abc', function (req, res)
     { res.sendFile(path.join(process.cwd(), 'abc.html'))
 })
-server.get ('*', (req, res)=> {
-    res.send(`<div>
-    <h1>404</h1>
-    <a href='/'> Ступай на главную страницу и не пиши чушь</a>
-    </div>`)
+
+server.get ('/index.html', function (req, res)
+    { res.sendFile(path.join(process.cwd(), 'index.html'))
 })
+
 server.post('/adressbook', function (req,res) {
+    let peopleFromDb = JSON.parse(fs.readFileSync('./src/data.json'));
     const dataFromFrom = req.body;
     // db.people.push(dataFromFrom);
     let name = dataFromFrom.name;
     let phone = dataFromFrom.phone;
-    db.people.push({
+    peopleFromDb.push({
         name: name,
         phone: phone
     });
+    fs.writeFileSync('./src/data.json', JSON.stringify(peopleFromDb));
     res.redirect('/');
 })
+
+server.get ('*', (req, res)=> {
+       res.render('404')
+    })
 
     // this.app.use(express.static(path.join(__dirname, "index.html")));
 server.listen(PORT)
@@ -41,3 +54,4 @@ console.log('http://127.0.0.1:3000')
 console.log('http://localhost:3000')
 console.log('http://localhost:3000/abc')
 console.log('http://localhost:3000/abcd прокатились на еже')
+console.log('http://localhost:3000/index.html')
